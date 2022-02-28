dd0fe4438537278ee4e7416fc1a486b1d8ad8338
LuisFelipeCracco <73305528+LuisFelipeCracco@users.noreply.github.com>
2022-02-28 08:35:15 -0300

    user greeting, photo, name

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,12 +1,33 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
-import { Container, Title } from './styles';
+import { 
+    Container,
+    Header,
+    UserInfo,
+    Photo,
+    User,
+    UserGreeting,
+    UserName,
+    UserWrapper
+} from './styles';
+
+
 
 export function Dashboard(){
     return(
         <>
             <Container>
-                <Title>Dashboard!!!</Title>
+                <Header>
+                    <UserWrapper>
+                        <UserInfo>
+                            <Photo source={{ uri: 'https://avatars.githubusercontent.com/u/73305528?v=4' }}/>
+                            <User>
+                                <UserGreeting>Olá,</UserGreeting>
+                                <UserName>Luis Felipe</UserName>
+                            </User>
+                        </UserInfo>
+                    </UserWrapper>
+                </Header>
             </Container>
         </>
     )
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,12 +1,60 @@
 import styled from 'styled-components/native';
+import { 
+    RFPercentage,
+    RFValue,
+} from 'react-native-responsive-fontsize'
 
 export const Container = styled.View`
     flex:1;
-    align-items: center;
-    justify-content: center;
-    background-color:#999;
+    background-color: ${({ theme }) => theme.colors.background};
 `;
 
-export const Title = styled.Text`
-    color: green;
+export const Header = styled.View`
+    width: 100%;
+    height: ${RFPercentage(42)}px;
+    background-color: ${({ theme }) => theme.colors.primary};
+    justify-content: center;
+    align-items: center;
+    flex-direction: row;
+    
 `;
+
+
+export const UserWrapper = styled.View`
+    width: 100%;
+    
+    align-self: flex-start;
+    padding: 25px 15px;
+`;
+
+export const UserInfo = styled.View`
+    flex-direction: row;
+    align-items: center;
+    
+    `;
+
+export const Photo = styled.Image`
+    width: ${RFValue(58)}px;
+    height: ${RFValue(58)}px;
+    border-radius: 10px;
+    `;
+
+export const User = styled.View`
+    margin-left: 17px;
+    
+    `;
+
+export const UserGreeting = styled.Text`
+    color: ${({ theme }) => theme.colors.shape};
+
+    font-size: ${RFValue(18)}px;
+    font-family: ${({theme}) => theme.fonts.regular};
+    `;
+
+export const UserName = styled.Text`
+    color: ${({ theme }) => theme.colors.shape};
+
+    font-size: ${RFValue(18)}px;
+    font-family: ${({theme}) => theme.fonts.bold};
+    `;
+
